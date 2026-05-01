49c2e04ab1c0bbfb3190f7fad898696cb31cbaed
ihriyad <ihriyad.dev@gmail.com>
2026-05-01 23:47:52 +0600

    Refactor code structure for improved readability and maintainability

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  //https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/11/15/09/cristiano-ronaldo-10.jpg
   reactCompiler: true,
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.independent.co.uk',
+        port: '',
+        pathname: '**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-
+import Image from "next/image";
+import React from "react";
+import ronaldo from "../../assets/images/ronaldo.webp"
 const AboutPage = () => {
-    return (
-        <div>
-            <h2>This is AboutPage</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is AboutPage</h2>
+      <p className="text-4xl text-center text-cyan-500 mb-6">The Goat Cristiano Ronaldo</p>
+      <div className="grid grid-cols-3 gap-2">
+        <Image
+          height={300}
+          width={300}
+          src="/kvicha.png"
+          alt="footballer"
+        ></Image>
+        <Image
+          height={300}
+          width={300}
+          src={ronaldo}
+          alt="footballer"
+        ></Image>
+        <Image
+          height={300}
+          width={300}
+          src="https://static.independent.co.uk/s3fs-public/thumbnails/image/2019/11/15/09/cristiano-ronaldo-10.jpg"
+          alt="footballer"
+        ></Image>
+      </div>
+    </div>
+  );
 };
 
 export default AboutPage;
